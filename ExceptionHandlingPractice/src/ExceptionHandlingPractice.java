30e8d1f4fbe0b0c0fd60ff82e8cd83ec0e9e758b
rasha <rasha@10.36.8.139>
2020-02-01 04:16:12 -0500

    Upload 1, experimenting with exceptions, try catch, custom exceptions, and finally.

---
+++ ExceptionHandlingPractice/src/ExceptionHandlingPractice.java
@@ -0,0 +1,92 @@
+import java.util.Scanner; 
+class NegativeNumberException extends Exception {
+	
+	private double number; 
+	
+	NegativeNumberException(String message, double number) {
+		super(message); 
+		this.number = number; 
+	}
+	
+	public double getNumber() {
+		return this.number; 
+	}
+	
+	public void setNumber(int number) {
+		this.number = number; 
+	}
+}
+
+
+
+
+class DivisionByZeroException extends Exception { 
+	private double number; 
+	
+	DivisionByZeroException(String message, double number ) {
+		super(message); 
+		this.number = number; 
+	}
+	
+	public double getNumber() {
+		return this.number; 
+	}
+	
+	public void setNumber(int number) {
+		this.number = number; 
+	}
+}
+
+class arithmaticCheck {
+	static void foo(double a) throws DivisionByZeroException, NegativeNumberException {
+		if (a ==0) {
+			throw new DivisionByZeroException("Division By Zero", a); 
+		}
+		else if ( a < 0) {
+			throw new NegativeNumberException("A cannot be Negative", a) ; 
+		}
+		
+		else {
+			System.out.println("The number is accepted: " + a) ; 
+		}
+	}
+}
+
+
+public class ExceptionHandlingPractice extends Exception{
+	
+	private double number; 
+	
+	public static void main (String args [])  throws NegativeNumberException, DivisionByZeroException{
+		
+		Scanner scan = new Scanner(System.in); 
+		
+		try {
+			double a = scan.nextDouble() ; 
+			arithmaticCheck.foo(a);
+			System.out.println("End Of Main");
+		}
+		catch (NegativeNumberException e) {
+			System.out.println(e.getMessage());
+			System.out.println("the number inputed was: " + e.getNumber()) ;
+			e.setNumber(1);
+			System.out.println("the number has been reset to: " + e.getNumber()) ; 
+		}
+		catch (DivisionByZeroException e) {
+			System.out.println(e.getMessage()); 
+			System.out.println("the number inputed was: " + e.getNumber()) ;
+			e.setNumber(1);
+			System.out.println("the number has been reset to: " + e.getNumber()) ; 
+		}
+		finally  {
+			System.out.println("End of Main");
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+}
